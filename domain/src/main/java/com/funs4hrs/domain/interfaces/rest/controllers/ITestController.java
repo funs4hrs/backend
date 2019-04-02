16ab5bb9816f94ac14b94c42f57c2cba13b325e4
Joris van Roy <j.vanroy@student.fontys.nl>
2019-04-02 16:24:50 +0200

    added forgotten files

--- domain/src/main/java/com/funs4hrs/domain/interfaces/rest/controllers/ITestController.java
+++ domain/src/main/java/com/funs4hrs/domain/interfaces/rest/controllers/ITestController.java
@@ -1,7 +1,8 @@
-package com.funs4hrs.domain.interfaces.rest;
+package com.funs4hrs.domain.interfaces.rest.controllers;
 
 import com.funs4hrs.domain.interfaces.ICRUD;
+import com.funs4hrs.domain.interfaces.rest.IRestCRUD;
 import com.funs4hrs.domain.models.Test;
 
-public interface ITestController extends ICRUD<Test,Long> {
+public interface ITestController extends IRestCRUD<Test,Long> {
 }
--- domain/src/main/java/com/funs4hrs/domain/interfaces/rest/controllers/IUserController.java
+++ domain/src/main/java/com/funs4hrs/domain/interfaces/rest/controllers/IUserController.java
@@ -1,9 +1,13 @@
-package com.funs4hrs.domain.interfaces.rest;
+package com.funs4hrs.domain.interfaces.rest.controllers;
 
 import com.funs4hrs.domain.interfaces.ICRUD;
+import com.funs4hrs.domain.interfaces.rest.IRestCRUD;
 import com.funs4hrs.domain.models.User;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-public interface IUserController extends ICRUD<User, Long> {
-    ResponseEntity Login(String email, String password);
+public interface IUserController extends IRestCRUD<User, Long> {
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    ResponseEntity login(String email, String password);
 }
